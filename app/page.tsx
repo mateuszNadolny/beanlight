138c6927ea178bc70d3fdce127c97d42872c4b22
mateuszNadolny <mateusznadolny00@gmail.com>
2025-03-04 22:13:26 +0100

    Add HowWeMakeCoffee section to homepage
    - Create new HowWeMakeCoffee component
    - Import and integrate component into main page
    - Add initial placeholder layout for section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import SmoothScrollHero from "@/components/hero/hero";
-
+import HowWeMakeCoffee from "@/components/how-we-make-coffee/how-we-make-coffee";
 export default function Home() {
   return (
     <main>
       <SmoothScrollHero />
+      <HowWeMakeCoffee />
     </main>
   );
 }
---
+++ components/how-we-make-coffee/how-we-make-coffee.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HowWeMakeCoffee = () => {
+  return (
+    <div className="w-full h-[200vh] bg-red-500">
+      <h1>How We Make Coffee</h1>
+    </div>
+  );
+};
+
+export default HowWeMakeCoffee;
